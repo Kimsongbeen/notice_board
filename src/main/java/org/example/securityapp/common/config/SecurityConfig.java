412edc1e2ece73f98d0c7ec86b4ecf9a2aec47e4
Kimsongbeen <ksb10266@gmail.com>
2025-12-20 00:15:16 +0900

    DAY1:SecurityConfig

---
+++ src/main/java/org/example/securityapp/common/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.example.securityapp.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
+
---
+++ src/test/java/org/example/securityapp/ConfigControllerTest.java
@@ -0,0 +1,5 @@
+package org.example.securityapp;
+
+public class ConfigControllerTest{
+
+}
